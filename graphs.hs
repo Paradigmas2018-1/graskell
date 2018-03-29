a0dbbe53e71982cfe1420e5f3526eae23049dfef
Arthur Assis <arthur120496@gmail.com>
2018-03-29 17:08:47 -0300

    creates neighbors function

--- graphs.hs
+++ graphs.hs
@@ -1,29 +1,17 @@
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Set (Set)
-import qualified Data.Set as Set
+import Data.Graph
+import Data.Function (on)
+import Data.List (sort)
 
---Nos do grafo
-type Vertex = Int
---Arestas do grafo, sempre entre dois nos
-type Edge = (Vertex,Vertex)
+graphGen :: (Int,Int) -> [(Int,Int)] -> Graph
+graphGen range ed = buildG range ed
 
---Uma lista de vizinhos de um grafo
-newtype Neighbors = Neighbors {vertex :: Set Vertex}
---O Grafo, Uma lista de listas de vizinhos 
-newtype Graph = Graph {neighs :: Map Vertex(Set Vertex)}
+neighbors' :: [(Int,Int)] -> Int -> [Int] -> [Int]
+neighbors' [] vertex aws = aws
+neighbors' ed vertex aws =
+    if fst(head ed) == vertex
+        then neighbors' (tail ed) vertex (aws ++ [snd(head ed)])
+        else neighbors' (tail ed) vertex aws
 
--- a representacao de um grafo vazio
-emptyGraph :: Graph
-emptyGraph = Graph Map.empty
-
--- a representacao de uma lista vizinhos vazia
-emptyNeighbors ::  Neighbors
-emptyNeighbors = Neighbors Set.empty
-
--- adicionando um elemento em um grafo, ainda nao tem nenhuma validacao
--- so funciona se o grafo estiver vazio
--- TODO: adicionar caso do grafo nao vazio
-addVertex( v, Neighbors n, Graph g) =   Map.insert v n g 
-    
+neighbors :: Graph -> Int -> [Int]
+neighbors g vertex = neighbors' (edges g) vertex []
 
